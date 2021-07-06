a512fbc14cc84ff03f039f9b5717820a74d65a11
rafaprates90 <84423386+rafaprates90@users.noreply.github.com>
2021-07-05 23:41:54 -0300

    Add files via upload

---
+++ funcoes-contrato-ethereum.js
@@ -0,0 +1,69 @@
+async function obterDadosDoContrato() {
+    let contractNumberField = document.getElementById("contractNumber");
+    const userInput = contractNumberField.value * 1;
+    alert("valorInformadoPeloUsuario: " + userInput);
+    try {
+      const arrayDadosDoContrato = await smartContract.contingencias(userInput);
+      console.log(arrayDadosDoContrato);
+      document.getElementById("nomeDaEmpresa").innerText = arrayRentalData[0];
+      document.getElementById("nomeDoEscritorio").innerText = arrayRentalData[1];
+      document.getElementById("periodoDeApuracao").innerText = arrayRentalData[2];
+      document.getElementById("processosAtivos").innerText = arrayRentalData[3];
+      document.getElementById("valorRemoto").innerText = arrayRentalData[4];
+      document.getElementById("valorPossivel").innerText = arrayRentalData[5];
+      document.getElementById("valorProvavel").innerText = arrayRentalData[6];
+      document.getElementById("valorTotal").innerText = arrayRentalData[7];
+    } catch (err) {
+      console.error(err);
+      document.getElementById("nomeDaEmpresa").innerText = "";
+      document.getElementById("nomeDoEscritorio").innerText = "";
+      document.getElementById("periodoDeApuracao").innerText = "";
+      document.getElementById("processosAtivos").innerText = "";
+      document.getElementById("valorRemoto").innerText = "";
+      document.getElementById("valorPossivel").innerText = "";
+      document.getElementById("valorProvavel").innerText = "";
+      document.getElementById("valorTotal").innerText = "";
+      contractNumberField.value = 0;
+      alert("Ops! Identificamos um erro ao buscar ao resultado da auditoria de numero: " + userInput);
+    }
+  }
+  
+  async function autoLoadOwner() {
+    try {
+      const contractOwner = await smartContract.owner();
+      console.log(contractOwner);
+      document.getElementById("spanOwner").innerText = contractOwner;
+    } catch (err) {
+      console.error(err);
+      alert("Atencao! Erro ao buscar o proprietario do contrato");
+    }
+  }
+  
+  async function saveFormData() {
+    try {
+      var tx;
+      var txReceipt;
+      tx = await smartContractWithSigner.registrarContingencias(
+        document.frmAuditoria.paramNomeDaEmpresa.value,
+        document.frmAuditoria.paramNomeDoEscritorio.value,
+        document.frmAuditoria.paramPeriodoDeApuracao.value,
+        document.frmAuditoria.paramProcessosAtivos.value,
+        document.frmAuditoria.paramValorRemoto.value,
+        document.frmAuditoria.paramValorPossivel.value,
+        document.frmAuditoria.paramValorProvavel.value,
+        document.frmAuditoria.paramValorTotal.value
+      );
+      console.log("transacao enviada ao metamask...Aguarde", tx);
+      alert("Transação enviada..." + tx.hash + " Aguarde a confirmação da Blockchain...");
+      txReceipt = await tx.wait();
+      console.log("transacao processada...", txReceipt);
+      if (txReceipt.status == 1) {
+        alert("Transação processada: " + tx.hash + "  - Registro salvo na Blockchain. Status: " + txReceipt.status);
+      } else {
+        alert("Transação processada: " + tx.hash + "  - Atencao! Houve um erro... Verifique através do Etherscan");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Houve um erro ao registrar o resultado da auditoria");
+    }
+  }
---
+++ informacoes-contrato-ethereum.js
@@ -0,0 +1,132 @@
+const contractAddress = "0x8cf94f347418944648e3d66eed4e5c25274914b8";
+var smartContract;
+var smartContractWithSigner;
+const ABI = [
+	
+            {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "contingencias",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "nomeDaEmpresa",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "nomeDoEscritorio",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "periodoDeApuracao",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "processosAtivos",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "valorRemoto",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "valorPossivel",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "valorProvavel",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "valorTotal",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "paramNomeDaEmpresa",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "paramNomeDoEscritorio",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "paramPeriodoDeApuracao",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "paramProcessosAtivos",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "paramValorRemoto",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "paramValorPossivel",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "paramValorProvavel",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "paramValorTotal",
+                    "type": "uint256"
+                }
+            ],
+            "name": "registrarContingencias",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
